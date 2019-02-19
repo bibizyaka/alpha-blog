e2587c4e191e10855b8112bc882dd1b45f0a1e88
gali k <gbmatrix@gmail.com>
2019-02-19 15:51:03 +0000

    add article category associaction

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,8 @@
 class Category < ActiveRecord::Base
   
+  has_many :article_categories
+  has_many :articles, through: :article_categories
+  
   validates :name, presence: true
   #validates :name, uniqueness: true
   validates_uniqueness_of :name
