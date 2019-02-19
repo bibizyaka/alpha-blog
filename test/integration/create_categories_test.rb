1d162d8c9fa771802aeddd3ce7c8298428db635d
gali k <gbmatrix@gmail.com>
2019-02-19 14:01:08 +0000

    Fix tests and update nav bar with categories

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -2,8 +2,12 @@ require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
   
+  def setup
+    @user = User.create(username: "Admin", email: "admin@admin.com", password: "password", admin: true)
+  end
+  
   test "get new category form and create category" do
-    
+    sign_in_as(@user, "password") # because it is hashed we need to proveide password as well
     get new_category_path
     assert_template 'categories/new'
     assert_difference 'Category.count', 1 do
@@ -15,6 +19,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
   end
   
   test "invalid category submission results in failure" do # trying to submit empty category name
+    sign_in_as(@user, "password")
   
     get new_category_path
     assert_template 'categories/new'
